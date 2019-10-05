553e8440d136116bc9cdd1446f409801106812ba
Tristan Maindron <tmaindron@gmail.com>
2019-10-05 20:38:31 +0200

    Initial work on art and gameplay basics

---
+++ Assets/Depth.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Renderer))]
+public class Depth : MonoBehaviour
+{
+    void Update()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.sortingOrder = -(int)(transform.position.y * 10f);
+    }
+}
---
+++ Assets/hero/Hero.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    float speed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = transform.position + Vector3.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = transform.position + Vector3.down * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = transform.position + Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = transform.position + Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.Find("hero").gameObject.GetComponent<Animator>().SetTrigger("jump");
+        }
+    }
+}
