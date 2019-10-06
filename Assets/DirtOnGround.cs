7c0fc2c4ab17e352620f71e4cd101aa6a073a893
Tristan Maindron <tmaindron@gmail.com>
2019-10-06 12:04:47 +0200

    Fire now melts snow

---
+++ Assets/DirtOnGround.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirtOnGround : MonoBehaviour
+{
+    float ellapsedTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ellapsedTime += Time.deltaTime;
+
+        GetComponent<SpriteRenderer>().color = Color.Lerp(new Color(1f, 1f, 1f, 0.1f), new Color(1f, 1f, 1f, 1f), ellapsedTime / 10f) ;
+    }
+}
---
+++ Assets/Fire.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public GameObject dirt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(dirt, transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
